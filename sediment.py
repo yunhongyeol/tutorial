076937d9bc65f736f0d0968cd5fac482eaca8c04
yunhongyeol <yunhongyul@gmail.com>
2021-07-31 15:57:48 +0900

    add rainfall

--- sediment.py
+++ sediment.py
@@ -1,6 +1,6 @@
-with open('./outputs/SEDIMENT(hd-0).OUT') as f:
+with open('./rainfalls/rainfall_daegu_2020.txt', 'rt', encoding='UTF8') as f:
     lines = f.readlines()
-
+    print(lines[-1])
 
 if __name__ == "__main__":
     pass
