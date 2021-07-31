9e5e5b940e4d4818c4aed4b2d062807c0ae94c49
Suyeol Yun <syyun@mit.edu>
2021-07-31 16:46:35 +0900

    add loop over text files

--- rainfall.py
+++ rainfall.py
@@ -1,3 +1,5 @@
+import glob
+
 def parse_date(text):
     """
     e.g. '20200112' -> 2020, 1, 12
@@ -19,33 +21,42 @@ def multiply_ten(all_day_rainfall):
     return result
 
 
-filename = "./rainfalls/rainfall_daegu_2020.txt"
+def convert_rainfall_format(filename):
+    # open file and read lines
+    with open(filename, "rt", encoding="UTF8") as f:
+        lines = f.readlines()
 
-with open(filename, "rt", encoding="UTF8") as f:
-    lines = f.readlines()
+    # open file to write and loop over lines read
+    output_filename = filename.replace(".txt", "") + "_c.txt"
 
-output_filename = filename.replace(".txt", "") + "_c.txt"
-print(output_filename)
-with open(output_filename, "w") as f:
-    for line in lines[1:]:
-        splits = line.split("|")
+    with open(output_filename, "w") as f:
+        for line in lines[1:]:
+            splits = line.split("|")
 
-        # position information
-        region_id_pos = 0
-        date_pos = 2
-        per_hour_rainfalls_pos_start = 3
-        per_hour_rainfalls_pos_end = 27
+            # position information
+            region_id_pos = 0
+            date_pos = 2
+            per_hour_rainfalls_pos_start = 3
+            per_hour_rainfalls_pos_end = 27
 
-        # data extraction
-        region_id = splits[region_id_pos]
-        year, month, day = parse_date(splits[date_pos])
-        rainfalls = multiply_ten(
-            splits[per_hour_rainfalls_pos_start:per_hour_rainfalls_pos_end]
-        )
-        new_row = [region_id, year, month, day] + rainfalls + ["\n"]
+            # data extraction
+            region_id = splits[region_id_pos]
+            year, month, day = parse_date(splits[date_pos])
+            rainfalls = multiply_ten(
+                splits[per_hour_rainfalls_pos_start:per_hour_rainfalls_pos_end]
+            )
+            new_row = [region_id, year, month, day] + rainfalls + ["\n"]
 
-        final = "|".join(new_row)
-        f.write(final)
+            final = "|".join(new_row)
+            f.write(final)
+
+
+rainfalls_dir = './rainfalls/*.txt'
+for file in glob.glob(rainfalls_dir):
+    if '_c' in file:
+        continue
+    else:
+        convert_rainfall_format(file)
 
 
 if __name__ == "__main__":
