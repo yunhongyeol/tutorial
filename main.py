ab9f468c9cbbffdc85dcdcbd55c4b1b83112074b
Suyeol Yun <syyun@mit.edu>
2021-07-31 14:29:53 +0900

    lint codes w/ python-black

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from PIL import Image
+from PIL.ExifTags import GPSTAGS
+from PIL.ExifTags import TAGS
+
 
 def get_exif(filename):
     image = Image.open(filename)
     image.verify()
     return image._getexif()
 
-exif = get_exif('test.jpg')
+
+exif = get_exif("test.jpg")
 print(exif)
 
-from PIL.ExifTags import GPSTAGS
 
 def get_geotagging(exif):
     if not exif:
@@ -16,7 +19,7 @@ def get_geotagging(exif):
 
     geotagging = {}
     for (idx, tag) in TAGS.items():
-        if tag == 'GPSInfo':
+        if tag == "GPSInfo":
             if idx not in exif:
                 raise ValueError("No EXIF geotagging found")
 
@@ -26,12 +29,14 @@ def get_geotagging(exif):
 
     return geotagging
 
+
 def get_coordinates(geotags):
-    lat = get_decimal_from_dms(geotags['GPSLatitude'], geotags['GPSLatitudeRef'])
+    lat = get_decimal_from_dms(geotags["GPSLatitude"], geotags["GPSLatitudeRef"])
 
-    lon = get_decimal_from_dms(geotags['GPSLongitude'], geotags['GPSLongitudeRef'])
+    lon = get_decimal_from_dms(geotags["GPSLongitude"], geotags["GPSLongitudeRef"])
 
-    return (lat,lon)
+    return (lat, lon)
+
 
 def get_decimal_from_dms(dms, ref):
 
@@ -39,16 +44,16 @@ def get_decimal_from_dms(dms, ref):
     minutes = dms[1] / 60.0
     seconds = dms[2] / 3600.0
 
-
-    if ref in ['S', 'W']:
+    if ref in ["S", "W"]:
         degrees = -degrees
         minutes = -minutes
         seconds = -seconds
 
     return round(degrees + minutes + seconds, 5)
 
-from PIL.ExifTags import TAGS
+
 geotags = get_geotagging(exif)
 print(get_coordinates(geotags))
+
 if __name__ == "__main__":
     pass
